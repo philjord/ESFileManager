26c51fa87ae46dd84a9718660d0b13b70e66a730
philip <philip@ihug.co.nz>
2013-09-03 22:28:14 +1200

    none

---
+++ ESMLoader/ca/davidfuchs/jessedit/ess/DateTime.java
@@ -0,0 +1,24 @@
+package ca.davidfuchs.jessedit.ess;
+
+/** terrible holder
+ * because I'm building for 1.6 for now!!!!!!!!!!!!
+ */
+public class DateTime
+{
+
+	public int getMillis()
+	{
+		return 0;
+	}
+
+	public DateTime plus(long l)
+	{
+		return this;
+	}
+
+	public DateTime toDateTime()
+	{
+		return this;
+	}
+
+}
