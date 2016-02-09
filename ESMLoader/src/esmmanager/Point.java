2691f58788fa4af31b162f039138dfa1d0f33287
phil <phil@Thinky>
2016-02-09 12:08:59 +0100

    Point is used as a key to maps, so need has and eq

--- ESMLoader/src/esmmanager/Point.java
+++ ESMLoader/src/esmmanager/Point.java
@@ -11,4 +11,21 @@ public class Point
 		this.y = y;
 	}
 
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (!(obj instanceof Point))
+			return false;
+		Point p = (Point) obj;
+		return x == p.x && y == p.y;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int bits = x;
+		bits ^= y * 31;
+		return (((int) bits) ^ ((int) (bits >> 32)));
+
+	}
 }
